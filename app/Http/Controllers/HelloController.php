60d087f992187f40240e0951c9c96d35cf1e3ca3
iwasaki-hub <66316321+iwasaki-hub@users.noreply.github.com>
2022-10-18 15:50:18 +0900

    2-15

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,33 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-
-// global 変数
-global $head, $style, $body, $end;
-$head='<html></head>';
-$style=<<<EOF
-<style>
-    body{
-        font-size: 16px;
-        color:#999;
-    }
-    h1{
-        font-size:100pt;
-        text-align:right;
-        color:#eee;
-        margin:-40px 0px -50px 0px;
-    }
-</style>
-EOF;
-$body= '</head><body>';
-$end='</body></html>';
-
-function tag($tag, $txt){
-    return "<{$tag}>" . $txt . "</{$tag}>";
-};
-
-
-
 class HelloController extends Controller
 {
    
